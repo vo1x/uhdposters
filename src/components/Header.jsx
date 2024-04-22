9b3fa740d417adb4933cfd414969b67b1dbcb88d
volx <97158976+vo1x@users.noreply.github.com>
2024-04-22 15:42:20 +0545

    Fix redirect url in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <Link to="/search/trending">
+    <Link to="/">
       <h1 className="text-3xl font-bold text-slate-100">UHDPosters</h1>
     </Link>
   );
