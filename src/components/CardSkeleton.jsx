271de193026ea8b5c562d1eb4c40d5599fc5aba2
volx <97158976+vo1x@users.noreply.github.com>
2024-04-27 08:46:10 +0545

    Update Skeleton Props

--- src/components/CardSkeleton.jsx
+++ src/components/CardSkeleton.jsx
@@ -2,9 +2,9 @@ import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { motion, AnimatePresence } from 'framer-motion';
 
-export default function CardSkeleton() {
+export default function CardSkeleton({ textSkeleton = true, initialAnimation = true }) {
   return (
-    <AnimatePresence>
+    <AnimatePresence initial={initialAnimation}>
       <motion.div
         initial={{ scale: 0.9, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
@@ -17,12 +17,14 @@ export default function CardSkeleton() {
           highlightColor="#334155"
           className="block h-full max-h-[168px] min-h-[168px] w-full min-w-28 max-w-28 md:max-h-72 md:min-h-72 md:min-w-48 md:max-w-48"
         />
-        <Skeleton
-          count={1}
-          baseColor="#1e293b"
-          highlightColor="#334155"
-          className="block h-5 w-24 md:bottom-5 md:min-w-40 md:max-w-40"
-        />
+        {textSkeleton && (
+          <Skeleton
+            count={1}
+            baseColor="#1e293b"
+            highlightColor="#334155"
+            className="block h-5 w-24 md:bottom-5 md:min-w-40 md:max-w-40"
+          />
+        )}
       </motion.div>
     </AnimatePresence>
   );
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,34 +3,36 @@ import { FiExternalLink } from 'react-icons/fi';
 import useClipboard from '../hooks/useClipboard';
 import { motion } from 'framer-motion';
 import GradientBackdrop from './GradientBackdrop';
-import { Video } from 'lucide-react';
 function DetailsPane(props) {
   const imageBaseUrl = 'https://image.tmdb.org/t/p/original';
+  const imagePrevUrl = 'https://image.tmdb.org/t/p/w220_and_h330_face';
   const { mediaInfo, imdbID, seasonsInfo, mediaType } = props;
-
   const [handleItemCopy] = useClipboard();
 
   return (
     <>
       <div className={`relative flex h-[500px] `}>
         <GradientBackdrop url={imageBaseUrl + mediaInfo.backdrop_path} />
-        <div className=" flex gap-5 place-self-end  px-10">
-          <div className="flex max-w-52 flex-col gap-1 text-center">
-            <img
-              src={imageBaseUrl + mediaInfo.poster_path}
-              alt="cover img"
-              className={`h-auto w-full min-w-52 max-w-52 rounded-md shadow-2xl shadow-slate-950  transition-opacity duration-300`}
-            />
+        <div className=" flex gap-5 place-self-end px-10">
+          <div className="relative mb-5 flex h-full max-h-96 min-h-80 min-w-52 max-w-52 flex-col gap-1 text-center">
             {mediaInfo && (
-              <motion.span
-                whileHover={{ color: '#7DD3FC' }}
-                className="cursor-pointer text-slate-300 "
-                onClick={(e) => handleItemCopy(e.target.innerText, 'Name & Date String')}
-              >
-                {`${mediaInfo.name || mediaInfo.title} (${mediaType === 'tv' && mediaInfo.first_air_date ? mediaInfo.first_air_date.split('-')[0] : mediaInfo.release_date ? mediaInfo.release_date.split('-')[0] : ''})`}
-              </motion.span>
+              <>
+                <img
+                  src={imagePrevUrl + mediaInfo.poster_path}
+                  alt="cover img"
+                  className={`h-auto w-full min-w-52 max-w-52 rounded-md shadow-2xl shadow-slate-950 transition-opacity duration-300`}
+                />
+                <motion.span
+                  whileHover={{ color: '#7DD3FC' }}
+                  className="cursor-pointer text-slate-300"
+                  onClick={(e) => handleItemCopy(e.target.innerText, 'Name & Date String')}
+                >
+                  {`${mediaInfo.name || mediaInfo.title} (${mediaType === 'tv' && mediaInfo.first_air_date ? mediaInfo.first_air_date.split('-')[0] : mediaInfo.release_date ? mediaInfo.release_date.split('-')[0] : ''})`}
+                </motion.span>
+              </>
             )}
           </div>
+
           <div className="flex flex-col justify-center gap-4 ">
             <div className="flex  flex-col justify-center gap-2">
               <div className="flex flex-col text-3xl ">
