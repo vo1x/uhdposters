ec1750db704c3ec4266993c61844bcf7e3d44b7b
vo1x <v0lx@icloud.com>
2024-08-22 16:10:32 +0000

    Fix Search Modal

--- src/components/UI/SearchModal.tsx
+++ src/components/UI/SearchModal.tsx
@@ -1,27 +1,22 @@
 import { useEffect, useRef, useState } from 'react';
-import { toast } from 'react-toastify';
-import axios from 'axios';
-import { useDebounce } from 'use-debounce';
-import { useQuery } from '@tanstack/react-query';
-import { Loader2, Search } from 'lucide-react';
-import { Link } from 'react-router-dom';
+import { Search } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 import { SetStateAction, Dispatch } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function SearchModal({
-  onChange
-}: {
+const SearchModal: React.FC<{
   onChange: ((value: string | boolean) => void) | Dispatch<SetStateAction<boolean>>;
-}) {
+}> = ({ onChange }) => {
+  const navigate = useNavigate();
+
   const [inputText, setInputText] = useState<string>('');
-  const [value] = useDebounce(inputText, 1000);
+
   const inputRef = useRef<HTMLInputElement>(null);
+
   const handleEscButton = () => {
     onChange(false);
   };
-
-  const [loading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const handleKeyDown = (e: any) => {
@@ -38,25 +33,6 @@ function SearchModal({
     };
   }, []);
 
-  const fetchInfo = async (query: string) => {
-    setIsLoading(true);
-    try {
-      const { data } = await axios.get(`/search?query=${query}`);
-      setIsLoading(false);
-      return data.results;
-    } catch (error: any) {
-      toast.error(error, { theme: 'colored', autoClose: 2000 });
-    }
-  };
-
-  const { data: searchResults, isFetched } = useQuery({
-    queryKey: [value],
-    queryFn: () => fetchInfo(value),
-    enabled: !!value || value !== '',
-    staleTime: Infinity,
-    refetchOnWindowFocus: false
-  });
-  const imageBaseUrl = 'https://image.tmdb.org/t/p/w94_and_h141_face';
   return (
     <AnimatePresence>
       <motion.div
@@ -82,7 +58,7 @@ function SearchModal({
             </button>
 
             <div className="absolute left-2 top-1/2 flex -translate-y-1/2 transform items-center justify-center rounded-md bg-slate-900  px-2 py-1 text-sm font-semibold text-slate-400">
-              <span>{loading ? <Loader2 className="animate-spin" /> : <Search />}</span>
+              <Search />
             </div>
 
             <motion.input
@@ -91,50 +67,20 @@ function SearchModal({
               placeholder="Search movie or series"
               value={inputText}
               onChange={(e) => setInputText(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  if (inputText.trim() !== '') {
+                    navigate(`/search/${inputText}`);
+                  }
+                }
+              }}
               className="w-full rounded-md border border-slate-600 bg-slate-900 p-4 pl-12  text-xl text-slate-300 placeholder-slate-500 shadow-md shadow-black/75 outline-none"
             />
           </div>
-          <AnimatePresence>
-            {isFetched && searchResults && searchResults.length > 0 && (
-              <motion.div
-                initial={{ scale: 0.95, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                exit={{ scale: 0.95, opacity: 0 }}
-                className="mt-2 max-h-[440px] overflow-y-auto rounded-lg border border-slate-600 bg-slate-900 text-slate-50 shadow-md shadow-black/75"
-              >
-                {searchResults.map((result: any, index: number) => (
-                  <Link key={index} to={`/details/${result.media_type}/${result.id}`}>
-                    <div
-                      onClick={() => onChange(false)}
-                      className="flex max-h-24 gap-2 rounded-lg p-2 hover:bg-slate-800"
-                    >
-                      <div className="min-w-12">
-                        <img
-                          src={imageBaseUrl + result.poster_path}
-                          width={48}
-                          loading="lazy"
-                          alt=""
-                        />
-                      </div>
-                      <div className="flex flex-col overflow-hidden">
-                        <span className="font-semibold">{result.title}</span>
-                        <span className="truncate text-sm text-slate-300">{result.overview}</span>
-                        <div className="flex gap-1 capitalize text-slate-400">
-                          <span>{result.media_type}</span>
-                          <span>•</span>
-                          <span>{result.release_date}</span>
-                        </div>
-                      </div>
-                    </div>
-                  </Link>
-                ))}
-              </motion.div>
-            )}
-          </AnimatePresence>
         </motion.div>
       </motion.div>
     </AnimatePresence>
   );
-}
+};
 
 export default SearchModal;
